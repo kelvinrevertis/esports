18928de2c38a5612f1a494737679b7e20a0732f7
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-09-28 17:38:09 -0300

    Count ads

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,21 @@ import  express, { response }  from "express";
 import { PrismaClient } from "@prisma/client";
 
 const app = express()
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+    log: ['query']
+})
 
-app.get('/games',(request, response) =>{
-    const games = prisma.game.findMany({})
-    return response.json([
-
-    ])
+app.get('/games',async (request, response) =>{
+    const games = await prisma.game.findMany({
+        include: {
+            _count:{
+                select: {
+                    ads: true,
+                }
+            }
+        }
+    })
+    return response.json(games)
 })
 
 app.post('/ads',(request, response) =>{
