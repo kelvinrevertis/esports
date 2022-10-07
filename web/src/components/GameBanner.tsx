734304c2d3b78b03ff3f882d13d84b7b6f5bf761
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-10-07 14:08:50 -0300

    Create Props

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -1,11 +1,17 @@
-export function GameBanner (){
+interface GameBannerProps{
+    bannerUrl: string;
+    title: string;
+    adsCount: string;
+}
+
+export function GameBanner (props: GameBannerProps){
     return(
         <a href="" className='relative rounded-lg overflow-hidden'>
-                    <img src="/Game (2).png" alt="" />
+                    <img src={props.bannerUrl} alt="" />
 
                     <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                        <strong className='font-bold text-white block'>Dota</strong>
-                        <span className=' text-zinc-300 text-sm block'> 6 anuncios</span>
+                        <strong className='font-bold text-white block'>{props.title}</strong>
+                        <span className=' text-zinc-300 text-sm block'> {props.adsCount}</span>
                     </div>
                 </a>
     )
