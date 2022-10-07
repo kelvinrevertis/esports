b2ca11d2588566938dcfc0818a2751b402fc9fc1
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-10-07 13:36:28 -0300

    Create components "a"

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { MagnifyingGlassPlus } from 'phosphor-react'
 import './styles/main.css'
 import LogoImg from './assets/logo-nlw-esports.svg'
+import { GameBanner } from './components/GameBanner'
 
 function App() {
     return (
@@ -12,50 +13,12 @@ function App() {
             </h1>
 
             <div className='grid grid-cols-6 gap-6 mt-16'>
-                <a href="" className='relative rounded-lg overflow-hidden'>
-                    <img src="/Game (1).png" alt="" />
-
-                    <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                        <strong className='font-bold text-white block'>League of Legends</strong>
-                        <span className=' text-zinc-300 text-sm block'> 4 anuncios</span>
-                    </div>
-                </a>
-                <a href="" className='relative rounded-lg overflow-hidden'>
-                    <img src="/Game (2).png" alt="" />
-
-                    <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                        <strong className='font-bold text-white block'>Dota</strong>
-                        <span className=' text-zinc-300 text-sm block'> 6 anuncios</span>
-                    </div>
-                </a>
-                <a href="" className='relative rounded-lg overflow-hidden'>
-                    <img src="/Game (3).png" alt="" />
-                    <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                        <strong className='font-bold text-white block'>CS GO</strong>
-                        <span className=' text-zinc-300 text-sm block'> 4 anuncios</span>
-                    </div>
-                </a>
-                <a href="" className='relative rounded-lg overflow-hidden'>
-                    <img src="/Game (4).png" alt="" />
-                    <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                        <strong className='font-bold text-white block'>Apex Legends</strong>
-                        <span className=' text-zinc-300 text-sm block'> 4 anuncios</span>
-                    </div>
-                </a>
-                <a href="" className='relative rounded-lg overflow-hidden'>
-                    <img src="/Game (5).png" alt="" />
-                    <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                        <strong className='font-bold text-white block'>Fortnite</strong>
-                        <span className=' text-zinc-300 text-sm block'> 4 anuncios</span>
-                    </div>
-                </a>
-                <a href="" className='relative rounded-lg overflow-hidden'>
-                    <img src="/Game (6).png" alt="" />
-                    <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                        <strong className='font-bold text-white block'>World of Warcraft</strong>
-                        <span className=' text-zinc-300 text-sm block'> 4 anuncios</span>
-                    </div>
-                </a>
+               <GameBanner/>
+               <GameBanner/>
+               <GameBanner/>
+               <GameBanner/>
+               <GameBanner/>
+               <GameBanner/>
 
             </div>
             <div className='pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden'>
---
+++ web/src/components/GameBanner.tsx
@@ -0,0 +1,12 @@
+export function GameBanner (){
+    return(
+        <a href="" className='relative rounded-lg overflow-hidden'>
+                    <img src="/Game (2).png" alt="" />
+
+                    <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+                        <strong className='font-bold text-white block'>Dota</strong>
+                        <span className=' text-zinc-300 text-sm block'> 6 anuncios</span>
+                    </div>
+                </a>
+    )
+}
