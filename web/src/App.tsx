24702ee41292f73511c1ded64ee9f0be377a9701
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-10-10 11:37:01 -0300

    using UseEffect

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,8 +9,10 @@ function App() {
     const [games, setGames] = useState([])
 
     useEffect(()=>{
-        
-    })
+        fetch('http://localhost:3000/games')
+        .then(response => response.json())
+        .then(data => {console.log(data)})
+    },[])
 
     
     return (
