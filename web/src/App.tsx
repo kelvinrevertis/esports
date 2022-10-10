63dabe867b6d4bce1d7686a350c5fdf5eb81a973
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-10-09 21:36:25 -0300

    AdBanner is a component now

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { MagnifyingGlassPlus } from 'phosphor-react'
 import './styles/main.css'
 import LogoImg from './assets/logo-nlw-esports.svg'
 import { GameBanner } from './components/GameBanner'
+import { CreateAdBanner } from './components/CreateAdBanner'
 
 function App() {
     return (
@@ -14,22 +15,10 @@ function App() {
 
             <div className='grid grid-cols-6 gap-6 mt-16'>
                <GameBanner bannerUrl='/Game (1).png' title='League of legends' adsCount={'8'}/>
-
-
+               <GameBanner bannerUrl='/Game (6).png' title='Wow' adsCount={'8'}/>
             </div>
-            <div className='pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden'>
-                <div className='bg-[#242634] px-8 py-6 flex justify-between items-center'>
-                    
-                    <div>
-                        <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-                        <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
-                    </div>
-                    <button className="px-4 py-3 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3">
-                        <MagnifyingGlassPlus size={24}/>
-                        Publicar anúncio
-                        </button>
-                </div>
-            </div>
+
+            <CreateAdBanner/>            
         </div>
     )
 }
