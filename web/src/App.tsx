08271fbb951677cc54b18335155e5a1b098c57a5
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-10-23 11:15:58 -0300

    Create Form

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -45,11 +45,44 @@ function App() {
                 <Dialog.Portal>
                     <Dialog.Overlay className='bg-black/60 inset-0 fixed'/>
 
-                    <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-                        <Dialog.Title>Publique um Anuncio</Dialog.Title>
+                    <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
+                        <Dialog.Title className='text-3xl font-black'>Publique um Anuncio</Dialog.Title>
 
                         <Dialog.Content>
-                            So pra vizualizar
+                            <form>
+                                <div>
+                                    <label htmlFor="game"> Qual o game?</label>
+                                    <input id='game' type="text" placeholder='Selecione o game que deseja jogar' />
+                                </div>
+                                <div>
+                                    <label htmlFor="name">Seu nome (ou nickname)</label>
+                                    <input id='name' placeholder='Como te chamam dentro do game?' />
+                                </div>
+                                <div>
+                                    <div>
+                                        <label htmlFor="yearsPlaying">Joga a quantos anos?</label>
+                                        <input id='yearsPlaying' type="number" placeholder='Tudo bem ser ZERO' />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="discord">Qual o seu Discord?</label>
+                                        <input type="discord" placeholder='Usuario#0000' />
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <div>
+                                        <label htmlFor="weekDays">Quando costuma jogar?</label>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="hourStart">QUal horario do dia?</label>
+                                    </div>
+                                    <div>
+                                        <input id='hourStart' type="time" placeholder='De'/>
+                                        <input id='hourEnd' type="time" placeholder='Até'/>
+                                    </div>
+                                </div>
+
+                            </form>
                         </Dialog.Content>
 
                     </Dialog.Content>
