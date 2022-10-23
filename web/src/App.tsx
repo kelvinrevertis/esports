cf2e448695733a09b9dd74b05a5383d8726be39a
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-10-22 23:14:11 -0300

    Centralize modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -45,7 +45,7 @@ function App() {
                 <Dialog.Portal>
                     <Dialog.Overlay className='bg-black/60 inset-0 fixed'/>
 
-                    <Dialog.Content>
+                    <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                         <Dialog.Title>Publique um Anuncio</Dialog.Title>
 
                         <Dialog.Content>
